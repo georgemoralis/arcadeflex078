7a91a0053e6f8edd32327545884d63d433c900e5
georgemoralis <giorgosmrls@gmail.com>
2022-09-30 07:19:28 +0300

    minimal 056 running minivadr

---
+++ emulator/src/mame056/driver.java
@@ -0,0 +1,19 @@
+/**
+ * ported to v0.56
+ */
+package mame056;
+
+import static mame056.driverH.*;
+
+import static mame056.drivers.minivadr.*;
+
+public class driver {
+
+    public static GameDriver drivers[] = {
+        /**
+         * Working
+         */
+        /*minivadr*/driver_minivadr,  
+        null
+    };
+}
