b2e102e2e59082fce705f7433c7683c64cdd5a01
georgemoralis <giorgosmrls@gmail.com>
2022-11-07 13:40:24 +0200

    shadow -> rewrote _4enraya vidhrdw

--- emulator/src/main/java/arcadeflex/v078/vidhrdw/_4enraya.java
+++ emulator/src/main/java/arcadeflex/v078/vidhrdw/_4enraya.java
@@ -1,25 +1,20 @@
-/** *************************************************************************
- *
- * vidhrdw.c
- *
- * Functions to emulate the video hardware of the machine.
- *
- ************************************************************************** */
-
 /*
  * ported to v0.78
- * using automatic conversion tool v0.01
+ * using automatic conversion tool v0.0.5
  */
 package arcadeflex.v078.vidhrdw;
 
-import static arcadeflex.v078.generic.funcPtr.*;
-import arcadeflex.v078.mame.drawgfxH.rectangle;
+//generic imports
+import arcadeflex.v078.generic.funcPtr.*;
+//mame imports 
+import static arcadeflex.v078.mame.drawgfxH.*;
 import static arcadeflex.v078.mame.tilemapC.*;
 import static arcadeflex.v078.mame.tilemapH.*;
-import static arcadeflex.v078.vidhrdw.generic.video_start_generic;
-import static arcadeflex.v078.vidhrdw.generic.videoram;
-import static arcadeflex056.fucPtr.*;
-import static mame056.commonH.*;
+//vidhrdw imports
+import static arcadeflex.v078.vidhrdw.generic.*;
+
+//TODO!
+import mame056.commonH.mame_bitmap;
 
 public class _4enraya {
 
@@ -27,16 +22,14 @@ public class _4enraya {
 
     public static WriteHandlerPtr fenraya_videoram_w = new WriteHandlerPtr() {
         public void handler(int offset, int data) {
-
             videoram.write((offset & 0x3ff) * 2, data);
             videoram.write((offset & 0x3ff) * 2 + 1, (offset & 0xc00) >> 10);
             tilemap_mark_tile_dirty(tilemap, offset & 0x3ff);
         }
     };
 
-    static GetTileInfoPtr get_tile_info = new GetTileInfoPtr() {
+    static GetTileInfoHandlerPtr get_tile_info = new GetTileInfoHandlerPtr() {
         public void handler(int tile_index) {
-            //System.out.println("get_tile_info!!!!");
             int code = videoram.read(tile_index * 2) + (videoram.read(tile_index * 2 + 1) << 8);
             SET_TILE_INFO(
                     0,
@@ -47,18 +40,15 @@ public class _4enraya {
     };
 
     public static VideoStartHandlerPtr video_start_4enraya = new VideoStartHandlerPtr() {
-        @Override
         public int handler() {
-            tilemap = (struct_tilemap) tilemap_create(get_tile_info, tilemap_scan_rows, TILEMAP_OPAQUE, 8, 8, 32, 32);
+            tilemap = tilemap_create(get_tile_info, tilemap_scan_rows, TILEMAP_OPAQUE, 8, 8, 32, 32);
             return video_start_generic.handler();
         }
     };
 
-    public static VhUpdatePtr video_update_4enraya = new VhUpdatePtr() {
+    public static VideoUpdateHandlerPtr video_update_4enraya = new VideoUpdateHandlerPtr() {
         public void handler(mame_bitmap bitmap, rectangle cliprect) {
-            tilemap_draw(bitmap, cliprect, tilemap, 0, 0);
             tilemap_draw(bitmap, cliprect, tilemap, 0, 0);
         }
     };
-
 }
